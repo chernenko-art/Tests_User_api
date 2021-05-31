bb93ae11a81332a29b727621b7a1337cab3d3a4e
Artem Chernenko <ac.chernenko@gmail.com>
2021-05-31 18:31:33 +0700

    Add a logging

---
+++ user_test_python/test_user_api.py
@@ -0,0 +1,21 @@
+import pytest
+import requests
+import logging
+import json
+
+path_json_file = '/home/chernenkoac/prog/test_users_api/user_test_python/config.json'
+
+
+def level_logging():
+    """Считывание level logging из config.json"""
+    json_file = open(path_json_file, 'r').read()
+    json_body = json.loads(json_file)
+    numeric_level = getattr(logging, json_body["log"]["level"].upper(), None)
+    return numeric_level
+
+#Конфигурация логов
+logging.basicConfig(level=level_logging(),
+                    format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename='user_tests.log'
+                    )
