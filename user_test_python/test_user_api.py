1b866fbbd67fe93fed74b1ad06da8439a992923a
Artem Chernenko <ac.chernenko@gmail.com>
2021-05-31 19:17:55 +0700

    The simple realize a test_doRegister

--- user_test_python/test_user_api.py
+++ user_test_python/test_user_api.py
@@ -1,5 +1,5 @@
 import pytest
-import requests
+from requests import get, post, put, delete
 import logging
 import json
 
@@ -19,3 +19,23 @@ logging.basicConfig(level=level_logging(),
                     datefmt='%m-%d %H:%M',
                     filename='user_tests.log'
                     )
+
+user_email = 'python_user@gmail.com'
+user_name = 'Reno'
+user_password = 123
+url = 'http://users.bugred.ru'
+
+
+def test_doRegister():
+    '''Тест регистрации пользователя'''
+    logging.info('Запуск do_register')
+    endpoint = '/tasks/rest/doregister'
+    response = post(url + endpoint, json = {
+        "email" : user_email,
+        "name": user_name,
+        "password": user_password
+    })
+    logging.debug(f'Post запрос {url + endpoint} вернул response header - {response.headers}')
+    response_body = response.json()
+    logging.debug(f'Post запрос {url + endpoint} вернул response body - {response.json()}')
+    assert 'type' not in response_body
