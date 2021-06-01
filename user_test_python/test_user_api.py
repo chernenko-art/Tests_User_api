3e8afb787bed4131b436b0cf6880429dfb6f412e
Artem Chernenko <ac.chernenko@gmail.com>
2021-06-01 22:21:11 +0700

    Add a timer to "test_doregister"

--- user_test_python/test_user_api.py
+++ user_test_python/test_user_api.py
@@ -1,8 +1,8 @@
-from types import ClassMethodDescriptorType
-from typing import ClassVar
-from requests import get, put, post, delete
+# from requests import get, put, post, delete
 import logging
-from conftest import level_logging, url_adress
+import time
+from conftest import level_logging,  url_adress
+from user_api import *
 
 
 # Конфигурация логов
@@ -14,77 +14,21 @@ logging.basicConfig(level=level_logging(),
                     )
 
 
-def check_response(response):
-    """Проверка header ответа сервера
-    """
-    logging.info('-'*15 + 'Проверка ответа от сервера')
-
-    # Проверка status_code
-    logging.info(f'Status code = {response.status_code}')
-    if response.status_code != 200:
-        logging.error(f'Status Code = {response.status_code}')
-    
-    # Проверка наличия 'application/json' в 'content-type'
-    content_type = response.headers['content-type']
-    logging.debug(f'Response headers: {response.headers}')
-    header_list = [elem.strip() for elem in content_type.split(';')]
-    if 'application/json' not in header_list:
-        logging.error(f'"application/json" отсутствует в "content-type"')
-        return False
-    else:
-        logging.debug(f'Response body = {response.json()}')
-        logging.debug(f'Header "content-type" = [{response.headers["content-type"]}]')
-        logging.info('-'*15 + 'Проверка ответа от сервера успешно выполнена')
-        return True
-
-
-def random_user_generator():
-    """Получение данных рандомного пользователя с помощью api https://randomuser.me/
-    """
-    logging.info('-'*15 + 'Запуск получения данных рандомного пользователя')
-    url = 'https://randomuser.me/api'
-    logging.info(f'Установка соединения с {url}')
-    response = get(url)
-    if check_response(response) == True:
-        response_body = response.json()
-        email = response_body['results'][0]['email']
-        logging.debug(f'Получен "email" : "{email}"')
-        name = response_body['results'][0]['login']['username']
-        logging.debug(f'Получен "name" : "{name}"')
-        password = response_body['results'][0]['login']['password']
-        logging.debug(f'Получен  "password" : "{password}"')
-        logging.info('-'*15 + f'Получены данные рандомного пользователя:\
-             "email" : "{email}", "name" : "{name}", "password" : "{password}"')
-        return email, name, password
-    else:
-        logging.error('-'*15 + 'Ошибка в получении данных рандомного пользователя')
-
-
-def doregister():
-    """Запрос на регистрацию пользователя
-    """
-    logging.info('-'*15 + 'Вызов метода doregister')
-    endpoint = '/tasks/rest/doregister'
-    email, name, password = random_user_generator()
-    logging.info(f'Установка соединения с {url_adress() + endpoint}')
-    response = post(url_adress() + endpoint, json={
-        "email": email,
-        "name": name,
-        "password": password
-    })
-    logging.info('-'*15 + f'Отправлен запрос на регистрацию пользователя:\
-         "name" : "{name}", "email" : "{email}", "password" : "{password}"')
-    if check_response(response) == True:
-        return response.json()
-    else:
-        logging.error('-'*15 + 'Ошибка в методе doregister')
-    
-
 def test_doregister():
-    """Тест метода doRegister (регистрация пользователя)
+    """
+    Тест метода doRegister
+
+    Тест реальзован при помощи проверки ответа об успешной регистрации 
+    в бесконечном цикле, для предотвращения зацикливания, при возникновении
+    непредвиденной ошибки используется таймер на 10 сек.     
+
     """
     logging.info('-'*15 + 'Запуск test_doregister')
+    timing = time.time() # заводим таймер
     while True:
+        if time.time() - timing > 10.0:  # проверка времени выполнения цикла
+            logging.error('Превышено время ожидания')
+            break
         response_body = doregister()
         if 'type' in response_body:
             logging.error('-'*15 + response_body[type])
@@ -96,4 +40,8 @@ def test_doregister():
             assert True
             break
 
+
+def test_createtask():
+    """Тест метода CreaTetask"""
+    pass
 
